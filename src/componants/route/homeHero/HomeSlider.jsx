d2cb07c2ef07fd15c0fc5774af50a606d128575d
hhshiblu <hhshiblu5555@gmail.com>
2024-02-09 14:05:47 +0600

    home2

--- src/componants/route/homeHero/HomeSlider.jsx
+++ src/componants/route/homeHero/HomeSlider.jsx
@@ -19,7 +19,7 @@ function HomeSlider({ banars }) {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2000000,
+          delay: 2000,
           disableOnInteraction: false,
         }}
         pagination={{
