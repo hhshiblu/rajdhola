2746c617e63bc0e5223815b9fff0f682200007f7
hhshiblu <hhshiblu5555@gmail.com>
2024-02-10 15:48:49 +0600

    j

--- src/componants/route/homeHero/HomeSlider.jsx
+++ src/componants/route/homeHero/HomeSlider.jsx
@@ -11,7 +11,12 @@ function HomeSlider({ banars }) {
           <img
             src="https://m.media-amazon.com/images/I/61HHa0VUuaL._SX3000_.jpg"
             alt=""
-            className="home_image 600px:mb-[-18%]  xl:mb-[-19%] mb-[-65px]"
+            className=" hidden md:block home_image 600px:mb-[-18%]  xl:mb-[-19%] mb-[-65px]"
+          />
+          <img
+            src="/short.jpg"
+            alt=""
+            className="home_image 600px:mb-[-210px] md:hidden  mb-[-95px]"
           />
         </div>
       ))}
